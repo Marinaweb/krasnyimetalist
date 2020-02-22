ab849d0fba3b9d77e8c6f0222035106e91f33987
Marina <ml.frontend@gmail.com>
2020-02-22 22:33:43 +0200

    Work for 22.02

--- front-page.php
+++ front-page.php
@@ -13,6 +13,15 @@ get_header();
 		<?php the_post(); ?>
 
 			<div class="content_front_page">
+
+				<?php 
+					if (pll_current_language() == 'ru') {			
+						echo do_shortcode('[rev_slider alias="home-ru"]'); 
+					} if (pll_current_language() == 'en') {
+						echo do_shortcode('[rev_slider alias="home-en"]'); 
+					}
+				?>
+
 				<?php the_content(); ?>
 			 </div><!--.content_front_page -->
 
